01d116bb11b9b45f01892ee0dab0df2f15e674e5
Eudes Nazário <eudesnazario178@gmail.com>
2024-12-01 19:19:53 +0000

    Dec 1, 2024, 4:19 PM

--- String/String ocorrendo em outra.c
+++ String/String ocorrendo em outra.c
@@ -11,21 +11,21 @@ int main() {
     scanf("%s", palavra2);
 
     // Calcula os tamanhos manualmente
-    for (len1 = 0; palavra1[len1] != '\0'; len1++);
-    for (len2 = 0; palavra2[len2] != '\0'; len2++);
+    for(len1 = 0; palavra1[len1] != '\0'; len1++);
+    for(len2 = 0; palavra2[len2] != '\0'; len2++);
 
     printf("Tamanho da palavra 1: %d\n", len1);
     printf("Tamanho da palavra 2: %d\n", len2);
 
     // Verifica se palavra2 está contida em palavra1
-    for (int i = 0; i <= len1 - len2; i++) {
+    for(int i = 0; i <= len1 - len2; i++) {
         int j;
-        for (j = 0; j < len2; j++) {
-            if (palavra1[i + j] != palavra2[j]) {
+        for(j = 0; j < len2; j++) {
+            if(palavra1[i + j] != palavra2[j]) {
                 break; // Sai do laço interno ao encontrar diferença
             }
         }
-        if (j == len2) { // Verifica se percorreu todo o comprimento de palavra2
+        if(j == len2) { // Verifica se percorreu todo o comprimento de palavra2
             iguais = len2;
             break;
         }
