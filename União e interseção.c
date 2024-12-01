53860549b0b1729ad41359ac5a6758b97e39bfb6
Eudes Nazário <eudesnazario178@gmail.com>
2024-12-01 02:43:20 +0000

    Nov 30, 2024, 11:43 PM

---
+++ União e interseção.c
@@ -0,0 +1,13 @@
+# include <stdio.h>
+
+int main(){
+    int X[100], Y[100], U[200];
+    for(int i = 0; i < 100; i++){
+        scanf("%d", &X[i]);
+    }
+    for(int i = 0;i < 100; i++){
+        scanf("%d", &Y[i]);
+    }
+    
+    return 0;
+}
