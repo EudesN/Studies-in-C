a87ddb4a9c7cdba90f24a5fe36fb07535c9b86ea
Eudes Nazário <eudesnazario178@gmail.com>
2024-12-01 21:00:13 +0000

    Dec 1, 2024, 6:00 PM

--- String/Concatenar strings.c
+++ String/Concatenar strings.c
@@ -3,18 +3,14 @@
 int main(){
     char str1[50] = "Ola, ";
     char str2[] = "Mundo!";
+
     int tam = 0, len1, len2;
 
     for(len1 = 0; str1[len1] != '\0'; len1++);
     for(len2 = 0; str2[len2] != '\0'; len2++);
 
-    for(int i = 0; str1 != '\0'; i++){
-        tam++;
-    }
-    for(int i = 0; str2 != '\0'; i++){
-        tam++;
-    }
-    char str3[tam];
+char str3[len1 +len2];
+
     for(int i = 0; i < len1; i++){
         str3[i] = str1[i];
     }
