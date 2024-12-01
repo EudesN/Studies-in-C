b13854e474c851b37730d06eb92e67fa488a2e60
Eudes Nazário <eudesnazario178@gmail.com>
2024-12-01 13:22:28 +0000

    Dec 1, 2024, 10:22 AM

--- String/Manipulando string.c
+++ String/Manipulando string.c
@@ -2,7 +2,11 @@
 
 int main(){
     char str[20];
+    printf("Digite algo(string convencional): ");
     scanf("%s", str);
-    printf("%s", str);
-    return 0;
+    printf("%s\n", str);
+
+    printf("Digite algo (string aprimorada): ");
+    scanf("%20[^\n]", str);
+    printf("Você digitou: %s\n", str);
 }
