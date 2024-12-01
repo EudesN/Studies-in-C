6280aa22c866e835baa14830bf1bbc25632d9c22
Eudes Nazário <eudesnazario178@gmail.com>
2024-12-01 13:33:04 +0000

    Dec 1, 2024, 10:33 AM

--- String/Manipulando string.c
+++ String/Manipulando string.c
@@ -5,6 +5,7 @@ int main(){
     printf("Digite algo(string convencional): ");
     scanf("%s", str);
     printf("%s\n", str);
+    fflush(stdin);
     for(int i = 0; i < 20; i++){
         str[i] = '\0';
     }
