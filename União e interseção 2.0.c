b9e19c3aa63656cf34eeb17407eab246bc60b279
Eudes Nazário <eudesnazario178@gmail.com>
2024-12-01 03:59:12 +0000

    Dec 1, 2024, 12:59 AM

--- União e interseção 2.0.c
+++ União e interseção 2.0.c
@@ -3,6 +3,7 @@
 int main(){
     int X[100], Y[100], U[200];
     int inter[100], exinter[100];
+
     for(int i = 0; i < 100; i++){
         scanf("%d", &X[i]);
         U[i] = X[i]; // copiando elementos de X para U na mesma posição de 0 a 99
@@ -18,10 +19,12 @@ int main(){
             U[100 + i] = Y[i]; // copiando elementos de Y para U na posição 100 a 199
         }
     }
+
     printf("Uniao: ");
     for(int i = 0;i < 200; i++){
         printf("%d ", U[i]);
     }
+    printf("\n");
     printf("Intersecao: ");
     int encontro = 0; // var que verifica se interseção existe
     for(int i = 0;i < 100; i++){
