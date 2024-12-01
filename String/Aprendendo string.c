fdeb0d188023983d8afec1aac512f8880b336f38
Eudes Nazário <eudesnazario178@gmail.com>
2024-12-01 13:50:16 +0000

    Dec 1, 2024, 10:50 AM

--- String/Aprendendo string.c
+++ String/Aprendendo string.c
@@ -1,7 +1,7 @@
 # include <stdio.h>
 
 int main(){
-    char str[7];
+    char str[7]; // vetor com capacidade para 6 caracteres + '\0'
     scanf("%6s", str); // scanf com limite para 6 caracteres
     printf("%s\n", str);
     return 0;
