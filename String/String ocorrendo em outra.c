c4cd4b3983c001b76dace042d934cebe24cc8d0b
Eudes Nazário <eudesnazario178@gmail.com>
2024-12-02 01:19:05 +0000

    Dec 1, 2024, 10:19 PM

--- String/String ocorrendo em outra.c
+++ String/String ocorrendo em outra.c
@@ -1,42 +1,30 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<string.h>
 
-int main() {
-    char palavra1[31], palavra2[31];
-    int len1, len2, iguais = 0;
-
-    // Entrada das palavras
-    printf("Informe a primeira palavra: ");
-    scanf("%s", palavra1);
-    printf("Informe a segunda palavra: ");
-    scanf("%s", palavra2);
-
-    // Calcula os tamanhos manualmente
-    for(len1 = 0; palavra1[len1] != '\0'; len1++);
-    for(len2 = 0; palavra2[len2] != '\0'; len2++);
-
-    printf("Tamanho da palavra 1: %d\n", len1);
-    printf("Tamanho da palavra 2: %d\n", len2);
-
-    // Verifica se palavra2 está contida em palavra1
-    for(int i = 0; i <= len1 - len2; i++) {
-        int j;
-        for(j = 0; j < len2; j++) {
-            if(palavra1[i + j] != palavra2[j]) {
-                break; // Sai do laço interno ao encontrar diferença
+int main (){
+    int cont=0;
+    char palavra1[64], palavra2[64];
+    scanf("%s", &palavra1);
+    scanf("%s", &palavra2);
+    for (int i = 0; i < strlen(palavra1); i++)
+    {
+        if (palavra1[i]!=palavra2[cont]){ // cont representa o indice, que começa no 0
+            cont=0;
+        }
+        if (palavra1[i]==palavra2[cont])
+        {
+            cont++; // se acha o =, acrescenta no cont e passa pra proxima letra
+            if (strlen(palavra2)== cont) 
+            {  /*Se o contador cont atingir o tamanho
+             da palavra2, significa que todos os 
+             caracteres da palavra2 foram encontrados
+              em sequência dentro da palavra1.*/
+                printf("ela esta dentro da outra");
+                return 0;
             }
+            
         }
-        if(j == len2) { // Verifica se percorreu todo o comprimento de palavra2
-            iguais = len2;
-            break;
-        }
+        
     }
-
-    // Resultado
-    if (iguais == len2) {
-        printf("Palavra 2 encontrada em 1\n");
-    } else {
-        printf("Palavra 2 nao encontrada\n");
-    }
-
-    return 0;
+        return 0;
 }
