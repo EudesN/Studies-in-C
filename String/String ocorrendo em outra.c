f116e223f7cadb141b743e196f844cd985beabea
Eudes Nazário <eudesnazario178@gmail.com>
2024-12-02 01:51:38 +0000

    Dec 1, 2024, 10:51 PM

--- String/String ocorrendo em outra.c
+++ String/String ocorrendo em outra.c
@@ -4,27 +4,21 @@
 int main (){
     int cont=0;
     char palavra1[64], palavra2[64];
-    scanf("%s", &palavra1);
-    scanf("%s", &palavra2);
-    for (int i = 0; i < strlen(palavra1); i++)
-    {
-        if (palavra1[i]!=palavra2[cont]){ // cont representa o indice, que começa no 0
-            cont=0;
+    scanf("%s", palavra1);
+    scanf("%s", palavra2);
+
+    for (int i = 0; i < strlen(palavra1); i++){
+        if (palavra1[i] != palavra2[cont]){ // cont representa o indice, que começa no 0
+            cont = 0;
         }
-        if (palavra1[i]==palavra2[cont])
-        {
+        if (palavra1[i]==palavra2[cont]){
             cont++; // se acha o =, acrescenta no cont e passa pra proxima letra
-            if (strlen(palavra2)== cont) 
-            {  /*Se o contador cont atingir o tamanho
-             da palavra2, significa que todos os 
-             caracteres da palavra2 foram encontrados
-              em sequência dentro da palavra1.*/
-                printf("ela esta dentro da outra");
+            if (strlen(palavra2)== cont) {  /*Se o contador cont atingir o tamanho da palavra2, significa que todos os caracteres da palavra2 foram encontrados em sequência dentro da palavra1.*/
+                printf("ela esta dentro da outra\n");
                 return 0;
             }
-            
         }
-        
     }
+    printf("Ele não está dentro da outra\n");
         return 0;
 }
