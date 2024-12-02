0e68d536bb7b5fa9b30fb1bcb640138546ee8ad9
Eudes Nazário <eudesnazario178@gmail.com>
2024-12-02 02:19:58 +0000

    Dec 1, 2024, 11:19 PM

--- String/String dentro de outra.c
+++ String/String dentro de outra.c
@@ -3,9 +3,11 @@
 int main (){
     int cont=0, len1, len2;
     char palavra1[31], palavra2[31];
+
     scanf("%s", palavra1);
     scanf("%s", palavra2);
-
+    
+// forma manual de contar o comprimento da string (percorre a string até o '\0')
 for(len1 = 0; palavra1[len1] != '\0'; len1++);
 for(len2 = 0; palavra2[len2] != '\0'; len2++);
 
@@ -13,9 +15,9 @@ for(len2 = 0; palavra2[len2] != '\0'; len2++);
         if (palavra1[i] != palavra2[cont]){ // cont representa o indice, que começa no 0
             cont = 0;
         }
-        if (palavra1[i] == palavra2[cont]){
+        else if (palavra1[i] == palavra2[cont]){
             cont++; // se acha o =, acrescenta no cont e passa pra proxima letra
-            if (len2 == cont) {  /*Se o contador cont atingir o tamanho da palavra2, significa que todos os caracteres da palavra2 foram encontrados em sequência dentro da palavra1.*/
+            if (len2 == cont) {  //Se o contador cont atingir o tamanho da palavra2, então todos os caracteres da palavra2 foram encontrados em sequência dentro da palavra1
                 printf("A segunda palavra esta dentro da primeira.\n");
                 return 0;
             }
