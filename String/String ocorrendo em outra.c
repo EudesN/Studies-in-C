fdad8db19b2fdf745af63f60d8baa0838a2c197a
Eudes Nazário <eudesnazario178@gmail.com>
2024-12-02 02:07:00 +0000

    Dec 1, 2024, 11:07 PM

--- String/String ocorrendo em outra.c
+++ String/String ocorrendo em outra.c
@@ -1,5 +1,4 @@
 #include<stdio.h>
-#include<string.h>
 
 int main (){
     int cont=0, len1, len2;
@@ -16,7 +15,7 @@ for(len2 = 0; palavra2[len2] != '\0'; len2++);
         }
         if (palavra1[i] == palavra2[cont]){
             cont++; // se acha o =, acrescenta no cont e passa pra proxima letra
-            if (strlen(palavra2)== cont) {  /*Se o contador cont atingir o tamanho da palavra2, significa que todos os caracteres da palavra2 foram encontrados em sequência dentro da palavra1.*/
+            if (len2 == cont) {  /*Se o contador cont atingir o tamanho da palavra2, significa que todos os caracteres da palavra2 foram encontrados em sequência dentro da palavra1.*/
                 printf("A segunda palavra esta dentro da primeira.\n");
                 return 0;
             }
