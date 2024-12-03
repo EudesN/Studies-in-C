e314fce7a25dc726b75be6c39ed13030e3854206
Eudes Nazário <eudesnazario178@gmail.com>
2024-12-03 13:04:44 +0000

    Dec 3, 2024, 10:04 AM

--- Vetor/Matriz Escada.c
+++ Vetor/Matriz Escada.c
@@ -1,11 +1,12 @@
 # include <stdio.h>
 
 int main(){
-    int M, N;
+    int M,N;
     
     scanf("%d", &M);
     scanf("%d", &N);
     int matriz[M][N];
+    int c_pivo[M]; // vetor que guarda os pivos de cada linha 
 
     for(int i = 0; i < M;i++){
         for(int j = 0; j < N; j++){
@@ -13,6 +14,12 @@ int main(){
         }
     }
 
-
+    for(int i = 0; i < M;i++){
+        for(int j = 0; j < N; j++){
+            if(matriz[i][j] != 0){
+                c_pivo[i] = j; // quardar o indice da coluna do pivo
+            }
+        }
+    }
     return 0;
 }
