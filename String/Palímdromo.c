c0f1e4320b85b4375aaa9e1353b74c2256803aa2
Eudes Nazário <eudesnazario178@gmail.com>
2024-12-05 01:05:29 +0000

    Dec 4, 2024, 10:05 PM

--- String/Palímdromo.c
+++ String/Palímdromo.c
@@ -7,16 +7,13 @@ int main(){
     scanf("%30s", str);
     for(len = 0; str[len] != '\0';len++);
 
-    for(int i = 0; i < len; i++){
-        for(int j = len - 1; j > 0; j++){
-            if(str[i] == str[j]){
-                cont++;
-                if(cont == len){
-                    printf("É um palíndromo!");
-                    return 0;
+    for(int i = 0; i < len / 2; i++){
+        if(str[i] == str[len - i] - 1){
+            if(cont == len){
+                printf("É um palíndromo!");
+                return 0;
                 }
             }
-        }
     }
     printf("Não é um palíndromo!");
     return 0;
