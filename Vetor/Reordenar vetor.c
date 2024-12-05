70f1854502f2b2888fbdb9bb998d5567ff3d7e2e
Eudes Nazário <eudesnazario178@gmail.com>
2024-12-05 02:27:11 +0000

    Dec 4, 2024, 11:27 PM

---
+++ Vetor/Reordenar vetor.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+
+int main(){
+    int x;
+    int len;
+    
+    printf("Informe o número de elementos no vetor: ");
+    scanf("%d", &x);
+    int vetor[x];
+
+    for(i = 0; i < x; i++){
+
+    }
+    return 0;
+}
