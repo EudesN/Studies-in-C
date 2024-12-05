39e1f9cd11a9964a7025172bfc3ad052df8a3ac2
Eudes Nazário <eudesnazario178@gmail.com>
2024-12-05 02:55:26 +0000

    Dec 4, 2024, 11:55 PM

--- Vetor/Reordenar vetor.c
+++ Vetor/Reordenar vetor.c
@@ -1,15 +1,37 @@
 # include <stdio.h>
 
 int main(){
-    int x;
-    int len;
-    
+    int tam;
+
     printf("Informe o número de elementos no vetor: ");
-    scanf("%d", &x);
-    int vetor[x];
+    scanf("%d", &tam);
+    int vetor[tam];
 
-    for(i = 0; i < x; i++){
+for(int i = 0; i < tam; i++){
+    scanf("%d", &vetor[i]);
+}
+// variáveis auxiliares
+int x = tam - 1, mudar; //x começa como a ultima posição do vetor e diminui a cada interação
+// mudar é o armazenamento temporario para a troca de valores
 
+for(int i = 0; i < tam; i++){
+    int entrou = 0;
+    for(int j = 0; j < x; j++){
+        if(vetor[j] > vetor[j + 1]){
+            mudar = vetor[j];
+            vetor[j] == vetor[j + 1];
+            vetor[j + 1] = mudar;
+            entrou = 1;
+
+        }
     }
+    x--;
+    if(entrou == 0){
+        break;
+    }
+}
+for(int i = 0; i < tam; i++){
+    printf("%d ", vetor[i]);
+}
     return 0;
 }
