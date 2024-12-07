0cd9830b66805c80e61bd8fee2559dfabc00e335
Eudes Nazário <eudesnazario178@gmail.com>
2024-12-07 21:23:35 +0000

    Dec 7, 2024, 6:23 PM

---
+++ OBI-2001/Calculando.c
@@ -0,0 +1,28 @@
+# include <stdio.h>
+# include <string.h>
+
+int main(){
+    int m, o, 
+
+while (1){
+    scanf("%d", &m);
+    if(m == 0){
+        break;
+    }
+    
+    char expre[200];
+    res = 0; // iniciar e zerar o resultado a cada interação
+
+    scanf("%s", expre);
+
+
+
+
+}
+
+
+
+
+
+    return 0;
+}
