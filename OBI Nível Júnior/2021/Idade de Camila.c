2f31d8b36387bbcaf68a92c1642d8dd4b89cfa31
Eudes Nazário <eudesnazario178@gmail.com>
2024-12-07 22:21:19 +0000

    Dec 7, 2024, 7:21 PM

--- OBI Nível Júnior/2021/Idade de Camila.c
+++ OBI Nível Júnior/2021/Idade de Camila.c
@@ -6,21 +6,15 @@ int main(){
     scanf("%d", &n1);
     scanf("%d", &n2);
     scanf("%d", &n3);
-    if(n2 < n3){
-        r = n2;
-        n2 = n3;
-        n3 = r;
+    // condicional que faz o valor ficar no meio( o que deve ser imprimido)
+    if((n1 > n2 && n1 < n3) || (n1 < n2 && n1 > n3)){
+        printf("%d\n", n1); 
     }
-    if(n1 < n2){
-        r = n1;
-        n1 = n2;
-        n1 = r;
+    else if((n2 > n1 && n2 < n3) || (n2 < n1 && n2 > n3)){
+        printf("%d\n", n2);
     }
-    if(n1 < n3){
-        r = n1;
-        n1 = n3;
-        n1 = r;
+    else{
+        printf("%d\n", n3);
     }
-    printf("%d", n3);
     return 0;
 }
