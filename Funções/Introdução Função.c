3f4cecedf983d9c54d9fbd1a8a33543e20afd24c
Eudes Nazário <eudesnazario178@gmail.com>
2024-12-10 21:42:10 +0000

    Dec 10, 2024, 6:42 PM

--- Funções/Introdução Função.c
+++ Funções/Introdução Função.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
+float calAreaRetan( float b, float h){
+    float area = b * h;
+    return area;
+}
+
 int main(){
-    
+    float x, y, area;
+    printf("Informe a base do retangulo: ");
+    scanf("%f", &x);
+    printf("Informe a altura do retangulo: ");
+    scanf("%f", &y);
+
+    area = calAreaRetan(x, y);  
+    printf("A area do retangulo e: %f", area);
     return 0;
 }
