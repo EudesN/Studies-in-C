60e989823a62e2c166c9c4a4213e3cf4d9d2ae6f
Eudes Nazário <eudesnazario178@gmail.com>
2024-12-11 02:12:18 +0000

    Dec 10, 2024, 11:12 PM

--- Funções/output/Variável Local Estática.c
+++ Funções/output/Variável Local Estática.c
@@ -1,7 +1,7 @@
 # include <stdio.h>
 
 void teste(void){
-    static int varLocalEstat = 2;
+    static int varLocalEstat = 2; // a cada vez q a função é chamada a variavel não é recriada e mantem o seu valor
     varLocalEstat *= 2;
     printf("%i\n", varLocalEstat);
 }
