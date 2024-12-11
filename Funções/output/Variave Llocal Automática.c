23b40195044fc31c188fd74a4c8fae4d687f4755
Eudes Nazário <eudesnazario178@gmail.com>
2024-12-11 02:05:34 +0000

    Dec 10, 2024, 11:05 PM

--- Funções/output/Variave Llocal Automática.c
+++ Funções/output/Variave Llocal Automática.c
@@ -2,7 +2,7 @@
 
 void teste(void){
     int varLocalAuto = 2; // a cada vez q a função 'teste' é chamada uma nova variavel é criada com valor '2' e seu valor não é mantido
-    varLocalAuto *= 2;
+    varLocalAuto *= 2; // essa variável somente é visivel dentro da função teste
     printf("%i\n", varLocalAuto);
 }
 
