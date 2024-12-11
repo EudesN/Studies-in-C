b395c23418b823786053373f257f4e2a73e6802c
Eudes Nazário <eudesnazario178@gmail.com>
2024-12-11 02:02:57 +0000

    Dec 10, 2024, 11:02 PM

--- Funções/output/Variavel local automática.c
+++ Funções/output/Variavel local automática.c
@@ -1,9 +1,9 @@
 # include <stdio.h>
 
 void teste(void){
-    int varLocalAuto = 2;
+    int varLocalAuto = 2; // a cada vez q a função 'teste' é chamada uma nova variavel é criada com valor '2' e seu valor não é mantido
     varLocalAuto *= 2;
-    printf("%d", varLocalAuto);
+    printf("%i\n", varLocalAuto);
 }
 
 int main(){
