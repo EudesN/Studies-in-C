8c66d456f63304dc6fa43dc6478d3fd409bb972c
Eudes Nazário <eudesnazario178@gmail.com>
2024-12-11 13:26:40 +0000

    Dec 11, 2024, 10:26 AM

---
+++ Funções/Funções que Chamam Outras Funções.c
@@ -0,0 +1,25 @@
+# include <stdio.h>
+
+float varAbsolut(float x){
+    return x * -1;
+}
+float somaDig(float num1, float num2){
+    float varAbsolut(float x);
+    if(num1 < 0){
+        num1 = varAbsolut(num1);
+    }
+    if(num2 < 0){
+        num2 = varAbsolut(num2);
+    }
+    return num1 + num2;
+}
+
+int main(){
+
+    float a, b, soma;
+    scanf("%f", &a);
+    scanf("%f", &b);
+    soma = somaDig(a, b);
+    printf("A soma e: %f", soma);
+    return 0;
+}
