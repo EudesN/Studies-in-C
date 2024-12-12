0f6bc62ef83b7ecdcb7b102bbc2d5f4a8cf6b599
Eudes Nazário <eudesnazario178@gmail.com>
2024-12-12 03:18:23 +0000

    Dec 12, 2024, 12:18 AM

--- Funções/Comportamento de Argumentos c/Funções.c
+++ Funções/Comportamento de Argumentos c/Funções.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
 
-void funPrint(int x, int vetor[]){
+void funPrint(int x, int vetor[]){ // cópia var x na função
+// já para vetores/matrizes não é uma cópia, mas sim o endereço da matriz na memória do computador
     x += 10; // x soma mais 10
     vetor[1] = 20; // valor do primeiro bloco alterado para 10
     printf("Variavel int na funcao principal = %i\n", x);
-    printf("Vetor na funcao principal = %i\n", vetor[1]);
+    printf("Vetor na funcao principal = %i\n\n", vetor[1]);
 }
 int main(){
     int x = 10;
     int vetor[3] = {10};
 
-    funPrint(x, vetor); // funPrint recebe a var x e o vetor 
+// funPrint recebe a var x e o vetor
+    funPrint(x, vetor);  // quando uma var é passada para uma função, quando a função é chamada, acontece uma cópia da var 
+// criando diferença entre a var recebida e a declarada, independentes entre si
 
     printf("Variavel int na funcao print = %i\n", x);
     printf("Vetor na funcao print = %i\n", vetor[1]);
