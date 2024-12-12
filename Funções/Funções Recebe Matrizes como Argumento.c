f75447df29418362088adedd8b0e364e46b5b1f9
Eudes Nazário <eudesnazario178@gmail.com>
2024-12-12 02:48:02 +0000

    Dec 11, 2024, 11:48 PM

---
+++ Funções/Funções Recebe Matrizes como Argumento.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void funPrint(int X[][3]){ // no mínimo a segunda parte da matriz é necessário informar, caso contrario gera erro
+    for(int i = 0; i < 3;i++){
+        for(int j = 0; j < 3;j++){
+            printf("%d", X[i][j]);
+        }
+    }
+}
+int main(){
+    int matriz[3][3] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+    funPrint(matriz);
+    return 0;
+}
