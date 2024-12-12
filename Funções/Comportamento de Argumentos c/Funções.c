d4f33ef6f5cfcd8b411f82a93af8785e561b1c5e
Eudes Nazário <eudesnazario178@gmail.com>
2024-12-12 03:04:05 +0000

    Dec 12, 2024, 12:04 AM

--- Funções/Comportamento de Argumentos c/Funções.c
+++ Funções/Comportamento de Argumentos c/Funções.c
@@ -3,8 +3,8 @@
 void funPrint(int x, int vetor[]){
     x += 10; // x soma mais 10
     vetor[1] = 20; // valor do primeiro bloco alterado para 10
-    printf("Variavel int na funcao print = %i\n", x);
-    printf("Vetor na funcao print = %i\n", vetor[1]);
+    printf("Variavel int na funcao principal = %i\n", x);
+    printf("Vetor na funcao principal = %i\n", vetor[1]);
 }
 int main(){
     int x = 10;
