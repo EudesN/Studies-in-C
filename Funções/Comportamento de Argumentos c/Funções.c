02a4b5fbf389f126a29c483e05e067c9e12eb575
Eudes Nazário <eudesnazario178@gmail.com>
2024-12-12 03:02:01 +0000

    Dec 12, 2024, 12:02 AM

--- Funções/Comportamento de Argumentos c/Funções.c
+++ Funções/Comportamento de Argumentos c/Funções.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
 void funPrint(int x, int vetor[]){
-    x += 10;
-    vetor[1] = 20;
-    printf("Variavel int na função print = %i\n", x);
-    printf("Vetor na função print = %i", vetor[1])
+    x += 10; // x soma mais 10
+    vetor[1] = 20; // valor do primeiro bloco alterado para 10
+    printf("Variavel int na funcao print = %i\n", x);
+    printf("Vetor na funcao print = %i\n", vetor[1]);
 }
 int main(){
     int x = 10;
-    int vetor[3] = 10;
+    int vetor[3] = {10};
 
-    funPrint(x, vetor);
+    funPrint(x, vetor); // funPrint recebe a var x e o vetor 
 
-    printf("Variavel int na função print = %i\n", x);
-    printf("Vetor na função print = %i", vetor[1])
-    
+    printf("Variavel int na funcao print = %i\n", x);
+    printf("Vetor na funcao print = %i\n", vetor[1]);
+
     return 0;
 }
