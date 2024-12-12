3ab3c2217cc5f52264b1fdae05d8c4a2fedc44ce
Eudes Nazário <eudesnazario178@gmail.com>
2024-12-12 02:08:14 +0000

    Dec 11, 2024, 11:08 PM

--- Funções/Funções Recebe Vetor como Argumento .c
+++ Funções/Funções Recebe Vetor como Argumento .c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 void OrdCresc(int vetor[], int n){
-    int temp;
+    int temp = 0;
 
-    for(int i = 0; i<n; i++){
+    for(int i = 0; i < n; i++){
         for(int j = i + 1; j < n; j++){
             if(vetor[i] > vetor[j]){
                 temp = vetor[i];
                 vetor[i] = vetor[j];
-                vetor[j] = vetor[i];
+                vetor[j] = temp;
             }   
         }
     }
@@ -16,11 +16,10 @@ void OrdCresc(int vetor[], int n){
 
 int main(){
     int vetor[]= {6, 4, 1, 9, 8, 0, 5, 7, 3, 2};
-    int i;
 
     OrdCresc(vetor, 10);
 
-    for(int i = 0; i < 10;i++){
-        printf("%i", vetor[i]);
+    for(int i = 0; i < 10; i++){
+        printf("%i ", vetor[i]);
     }
 }
