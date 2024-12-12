97c9cb7cf32ffd5157cc1b46598db68ca147ce37
Eudes Nazário <eudesnazario178@gmail.com>
2024-12-12 04:28:33 +0000

    Dec 12, 2024, 1:28 AM

--- Struct/Passar e Receber Estruturas à uma Função.c
+++ Struct/Passar e Receber Estruturas à uma Função.c
@@ -1,14 +1,21 @@
 #include <stdio.h>
 
-int main(){
     struct horario{
         int horas;
         int min;
         int seg;
     };
+
+struct horario teste(struct horario x){
+
+}
+int main(){
+
+
     struct horario agora;
     agora.horas = 10;
     agora.min = 42;
     agora.seg = 58;
+
     return 0;
 }
