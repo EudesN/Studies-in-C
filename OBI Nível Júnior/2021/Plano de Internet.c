3800696f3966a0dbf07d88f2885d898eee6b0016
Eudes Nazário <eudesnazario178@gmail.com>
2024-12-13 12:54:05 +0000

    Dec 13, 2024, 9:54 AM

--- OBI Nível Júnior/2021/Plano de Internet.c
+++ OBI Nível Júnior/2021/Plano de Internet.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
 
 int main(){
-    
+    int X,N;
+    int R = 0,S;
+
+    scanf("%d", &X);
+    scanf("%d", &N);
+    int M[N];
+
+    for(int i = 0;i < N;i++){
+        scanf("%d", &M[i]);
+    }
+    for(int i = 0;i < N;i++){
+        S =X + R;
+        R = S - M[i];
+    }
+    printf("%d", S);
+    return 0;
 }
