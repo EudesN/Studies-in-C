a7cb616717dc3eb30edd823f72f79e63a28ed5fa
Eudes Nazário <eudesnazario178@gmail.com>
2024-12-14 18:25:08 +0000

    Dec 14, 2024, 3:25 PM

--- OBI Nível Júnior/2021/Torneio de tênis.c
+++ OBI Nível Júnior/2021/Torneio de tênis.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
 
 int main(){
-    
+    int v = 0;
+    char part;
+
+    for(int i = 0; i < 6;i++){
+        scanf("%c", &part);
+        if(part == 'v'){
+            v++;
+        }
+    }
+    if(v == 5 || v == 6){
+        printf("1\n");
+    }
+    else if(v == 3 || v == 4){
+        printf("2\n");
+    }
+    else if(v == 1 || v == 2){
+        printf("3\n");
+    }
+    else{
+        printf("-1\n");
+    }
+
     return 0;
 } 
