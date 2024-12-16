ac4d3748c1022583385a0efcc2ae6b929e0a070f
Eudes Nazário <eudesnazario178@gmail.com>
2024-12-16 01:24:10 +0000

    Dec 15, 2024, 10:24 PM

--- OBI Nível Júnior/2021/2 fase/Anagrama.c
+++ OBI Nível Júnior/2021/2 fase/Anagrama.c
@@ -1,19 +1,33 @@
-    # include <stdio.h>
+#include <stdio.h>
 
-    int main(){
-        int N;
-        scanf("%d", &N);
-        getchar();
-        char P1[N + 1], P2[N + 1];
-        fgets(P1, sizeof(P1), stdin);
-        for(int i = 0; P1[i] != '\0';i++){
-            if(P1[i]== '\n'){
-                P1[i] = '\0';
-                break;
-            }
+int main() {
+    int N;
+    scanf("%d", &N);
+    getchar(); // Limpa o '\n' do buffer após o scanf
+
+    char P1[N + 1], P2[N + 1];
+
+    // Lê a primeira string
+    fgets(P1, sizeof(P1), stdin);
+    for (int i = 0; P1[i] != '\0'; i++) {
+        if (P1[i] == '\n') {
+            P1[i] = '\0'; // Remove o '\n' se presente
+            break;
         }
-        fgets(P2, sizeof(P2), stdin);
-        printf("%s\n", P1);
-        printf("%s\n", P2);
-        return 0;
     }
+
+    // Lê a segunda string
+    fgets(P2, sizeof(P2), stdin);
+    for (int i = 0; P2[i] != '\0'; i++) {
+        if (P2[i] == '\n') {
+            P2[i] = '\0'; // Remove o '\n' se presente
+            break;
+        }
+    }
+
+    // Imprime as strings
+    printf("%s\n", P1);
+    printf("%s\n", P2);
+
+    return 0;
+}
