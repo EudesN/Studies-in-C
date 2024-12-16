1bd0080cbfe7fa59ede2624be4590acee1c9076a
Eudes Nazário <eudesnazario178@gmail.com>
2024-12-16 01:38:45 +0000

    Dec 15, 2024, 10:38 PM

--- OBI Nível Júnior/2021/2 fase/Anagrama.c
+++ OBI Nível Júnior/2021/2 fase/Anagrama.c
@@ -7,7 +7,7 @@
         char P1[N + 1], P2[N + 1];
 
         printf("Informe o primeira palavra: ");
-        fgets(P1, sizeof(P1), stdin);
+        fgets(P1, sizeof(P1 + 1), stdin);
 
         for(int i = 0; P1[i] != '\0';i++){
             if(P1[i]== '\n'){
@@ -16,7 +16,7 @@
             }
         }
         printf("Informe o segunda palavra: ");
-        fgets(P2, sizeof(P2), stdin);
+        fgets(P2, sizeof(P2 + 1), stdin);
 
         for(int i = 0; P2[i] != '\0';i++){
             if(P2[i]== '\n'){
