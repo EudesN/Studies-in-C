6fd02cf3cffc00e4eee62649b9859ef4d4a094b1
Eudes Nazário <eudesnazario178@gmail.com>
2024-12-16 01:35:43 +0000

    Dec 15, 2024, 10:35 PM

--- OBI Nível Júnior/2021/2 fase/Anagrama.c
+++ OBI Nível Júnior/2021/2 fase/Anagrama.c
@@ -1,31 +1,23 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
     int N;
     scanf("%d", &N);
-    getchar(); // Limpa o '\n' do buffer após o scanf
+    getchar(); // Remove o '\n' restante no buffer após ler N
 
     char P1[N + 1], P2[N + 1];
 
-    // Lê a primeira string
-    fgets(P1, sizeof(P1), stdin);
-    for (int i = 0; P1[i] != '\0'; i++) {
-        if (P1[i] == '\n') {
-            P1[i] = '\0'; // Remove o '\n' se presente
-            break;
-        }
-    }
+    // Lendo a primeira string
+    fgets(P1, N + 1, stdin);
+    P1[strcspn(P1, "\n")] = '\0'; // Remove o '\n' (se existir)
+    getchar(); // Remove qualquer caractere residual no buffer (como o '\n')
 
-    // Lê a segunda string
-    fgets(P2, sizeof(P2), stdin);
-    for (int i = 0; P2[i] != '\0'; i++) {
-        if (P2[i] == '\n') {
-            P2[i] = '\0'; // Remove o '\n' se presente
-            break;
-        }
-    }
+    // Lendo a segunda string
+    fgets(P2, N + 1, stdin);
+    P2[strcspn(P2, "\n")] = '\0'; // Remove o '\n' (se existir)
 
-    // Imprime as strings
+    // Imprime as duas strings
     printf("%s\n", P1);
     printf("%s\n", P2);
 
