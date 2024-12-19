02058d18deffa73211ebb6c00975a3c2b8707aa2
Eudes Nazário <eudesnazario178@gmail.com>
2024-12-19 15:00:06 +0000

    Dec 19, 2024, 12:00 PM

--- Struct/Revisão struct.c
+++ Struct/Revisão struct.c
@@ -4,9 +4,23 @@ struct horario{
     int horas;
     int min;
     int seg;
-}
+};
 
+void receberHorarios(struct horario list[5]){
+    for(int i = 0; i < 5; i++){
+        printf("Informe o %i horario(hh:mm:ss): ", i + 1);
+        scanf("%d", list[5])
+    }   
+}
+void printfHorarios(struct horario list[5]){
+
+}
 int main(){
+    struct horario listaHorarios[5]; 
+
+    receberHorarios(listaHorarios);
+
+    printfHorarios(listaHorarios);
 
     return 0;
 }
