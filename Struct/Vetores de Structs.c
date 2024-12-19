eba3f77370962e4527a1d2aa05567898bbda56b5
Eudes Nazário <eudesnazario178@gmail.com>
2024-12-19 14:20:08 +0000

    Dec 19, 2024, 11:20 AM

---
+++ Struct/Vetores de Structs.c
@@ -0,0 +1,18 @@
+# include <stdio.h>
+
+int main(){
+    struct horario{
+        int horas;
+        int min;
+        int seg;
+    };
+    
+    struct horario teste[5];
+
+    teste[0].horas = 10;
+    teste[0].min = 20;
+    teste[0].seg = 30;
+
+    printf("%i : %i : %i\n", teste[0].horas, teste[0].min, teste[0].seg);
+    return 0; 
+}
