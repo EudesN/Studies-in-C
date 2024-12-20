9fdd0ecba95f6dd5d335c651e6919f9120fa9cf8
Eudes Nazário <eudesnazario178@gmail.com>
2024-12-20 21:33:33 +0000

    Dec 20, 2024, 6:33 PM

--- Struct/Revisão struct.c
+++ Struct/Revisão struct.c
@@ -1,19 +1,21 @@
 # include <stdio.h>
 
 struct horario{
-    int horas;
+    int hora;
     int min;
     int seg;
 };
 
-void receberHorarios(struct horario list[5]){
+void receberHorarios(struct horario lista[5]){
     for(int i = 0; i < 5; i++){
         printf("Informe o %i horario(hh:mm:ss): ", i + 1);
-        scanf("%d", list[5])
+        scanf("%i : %i : %i", &lista[i].hora, &lista[i].min, &lista[i].seg);
     }   
 }
-void printfHorarios(struct horario list[5]){
-
+void printfHorarios(struct horario lista[5]){
+    for(int i = 0; i < 5; i++){
+        printf("O %i horario e %i : %i : %i", i + 1, lista[i].hora, lista[i].min, lista[i].seg);
+    }
 }
 int main(){
     struct horario listaHorarios[5]; 
