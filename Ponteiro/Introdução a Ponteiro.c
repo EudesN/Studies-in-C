b74c5230e5a0a9cb56ea20eab7768b600ce7d166
Eudes Nazário <eudesnazario178@gmail.com>
2024-12-21 20:10:56 +0000

    Dec 21, 2024, 5:10 PM

--- Ponteiro/Introdução a Ponteiro.c
+++ Ponteiro/Introdução a Ponteiro.c
@@ -1,6 +1,9 @@
 # include <stdio.h>
 
 int main(){
+    int x; // reserva um espaço na memória para a variável inteira x 
+    x = 10; // nesse espaço de memória reservado para x é armazenado o valor 10
 
+    printf("%i\n", &x);
     return 0;
 }
