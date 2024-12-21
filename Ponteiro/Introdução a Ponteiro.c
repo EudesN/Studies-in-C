4a33f5941da0621cd33b212c07f9dfea2937d2b3
Eudes Nazário <eudesnazario178@gmail.com>
2024-12-21 20:12:55 +0000

    Dec 21, 2024, 5:12 PM

--- Ponteiro/Introdução a Ponteiro.c
+++ Ponteiro/Introdução a Ponteiro.c
@@ -4,6 +4,11 @@ int main(){
     int x; // reserva um espaço na memória para a variável inteira x 
     x = 10; // nesse espaço de memória reservado para x é armazenado o valor 10
 
-    printf("%i\n", &x);
+    int *ponteiro;
+    ponteiro  = &x;
+
+    printf("%i\n", *ponteiro);
+
+    // printf("%i\n", &x); // o '&' é o operador de endereço, ele retorna o endereço de memória da variável x
     return 0;
 }
