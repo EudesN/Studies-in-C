80a1504f02c79922029b3eae71eb64565a6621af
Eudes Nazário <eudesnazario178@gmail.com>
2024-12-26 01:09:54 +0000

    Dec 25, 2024, 10:09 PM

--- Ponteiro/Ponteiros char, int, double.c
+++ Ponteiro/Ponteiros char, int, double.c
@@ -9,9 +9,12 @@ int main(){
     double *pY = &y;
     char *pZ = &z;
 
-    printf("Endereço x = %p - Valor  x = %d\n", (void*)pX, *pX );
-    printf("Endereço y = %p - Valor y = %f\n", (void*)pY, *pY);
-    printf("Endereço z = %p - Valor z = %c\n", (void*)pZ, *pZ);
+    double soma = *pX + *pY;
+
+    printf("Endereco x = %p - Valor  x = %d\n", (void*)pX, *pX );
+    printf("Endereco y = %p - Valor y = %.1f\n", (void*)pY, *pY);
+    printf("Endereco z = %p - Valor z = %c\n", (void*)pZ, *pZ);
+    printf("Valor de soma = %f", soma);
 
     return 0;
 }
