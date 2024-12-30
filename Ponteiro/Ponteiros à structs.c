2e680d701a239452dc90f9285c0e5891f9e533ef
Eudes Nazário <eudesnazario178@gmail.com>
2024-12-30 02:58:07 +0000

    Dec 29, 2024, 11:58 PM

--- Ponteiro/Ponteiros à structs.c
+++ Ponteiro/Ponteiros à structs.c
@@ -3,15 +3,18 @@
 int main(){
 
     struct horario{
-        int horas;
+        int hora;
         int min;
         int seg;
     };
     struct horario agora, *depois;
 
     depois = &agora;
+    (*depois).hora = 20;
+    (*depois).min = 20;
+    (*depois).seg = 20;
 
-    printf("%i:%i:%i", agora.horas, agora.min, agora.seg);
+    printf("%i:%i:%i", agora.hora, agora.min, agora.seg);
     
     return 0;
 }
