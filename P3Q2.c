c5851be57ae5c3a45731f6970c57218d1f66f03d
Eudes Nazário <eudesnazario178@gmail.com>
2025-01-06 21:05:52 +0000

    Jan 6, 2025, 6:05 PM

--- P3Q2.c
+++ P3Q2.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
-
-int main(){
     struct aluno{
         int matricula;
         float notaP1;
         float notaP2;
-    }
+    };
     struct aluno tAluno;
-    
+
+int main(){
+
+
     return 0;
 }
