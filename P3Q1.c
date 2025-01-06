759cd0b8ffe39715e4b1f423aaa0a25c93d7cb2c
Eudes Nazário <eudesnazario178@gmail.com>
2025-01-06 13:48:00 +0000

    Jan 6, 2025, 10:48 AM

--- P3Q1.c
+++ P3Q1.c
@@ -1,14 +1,29 @@
 # include <stdio.h>
 
 int main(){
-    int M[50][12];
+    int M[50][12], faltasMes[12], faltasAluno[50];
     int cont = 0;
 
     for(int l = 0; l < 50; l++){
         for(int c = 0; c < 12; c++){
-            printf("Informe o número de faltas do aluno %d: ");
+            printf("Informe o número de faltas do aluno %d: ", l + 1);
             scanf("%d", &M[l][c]);
         }
     }
+    // total de faltas em cada mês
+    
+    for(int c = 0; c < 12; c++){
+        for(int l = 0; l < 50; l++){
+            faltasMes[c] = M[l][c];
+        }
+    }
+    // total de faltas de cada aluno
+    for(int l = 0; l < 50; l++){
+        for(int c = 0; c < 12; c++){
+            faltasAluno[l] = M[l][c];
+        }
+    }
+    // qual ou quais alunos tiveram o maior número de faltas
+    
     return 0;
 }
