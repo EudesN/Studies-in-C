d5671afb26ee0bf84621b5e38f158059a19f415f
Eudes Nazário <eudesnazario178@gmail.com>
2025-01-06 14:18:02 +0000

    Jan 6, 2025, 11:18 AM

--- P3Q1.c
+++ P3Q1.c
@@ -1,8 +1,7 @@
 # include <stdio.h>
 
 int main(){
-    int M[50][12], faltasMes[12], faltasAluno[50];
-    int cont = 0;
+    int M[50][12], faltasMes[12] = {0}, faltasAluno[50] = {0};
 
     for(int l = 0; l < 50; l++){
         for(int c = 0; c < 12; c++){
@@ -11,19 +10,43 @@ int main(){
         }
     }
     // total de faltas em cada mês
-    
     for(int c = 0; c < 12; c++){
         for(int l = 0; l < 50; l++){
-            faltasMes[c] = M[l][c];
+            faltasMes[c] += M[l][c];
         }
     }
+
+    printf("Faltas em cada mes:\n");
+    for(int c = 0; c < 12; c++){
+        for(int l = 0; l < 50; l++){
+            printf("Mes %d: %d\n", c + 1, faltasMes[c]);
+        }
+    }
+
     // total de faltas de cada aluno
     for(int l = 0; l < 50; l++){
         for(int c = 0; c < 12; c++){
-            faltasAluno[l] = M[l][c];
+            faltasAluno[l] += M[l][c];
+        }
+    }
+    printf("Faltas por aluno:\n");
+    for(int l = 0; l < 50; l++){
+        for(int c = 0; c < 12; c++){
+            printf("Aluno %d: %d\n", l + 1, faltasAluno[l]);
         }
     }
     // qual ou quais alunos tiveram o maior número de faltas
-    
+    printf("Aluno(s) com maior numero de faltas:\n");
+    int maior = 0;
+    for(int i = 0; i < 50; i++){
+        if(faltasAluno[i] > maior){
+            maior = faltasAluno[i];
+        }
+    }
+    for(int i = 0; i < 50; i++){
+        if(faltasAluno[i] == maior){
+            printf("Aluno %d\n", i + 1);
+        }
+    }
     return 0;
 }
