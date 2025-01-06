bf7ea20ae04bdb29a704f302d781cd808133c125
Eudes Nazário <eudesnazario178@gmail.com>
2025-01-06 02:00:42 +0000

    Jan 5, 2025, 11:00 PM

--- Ponteiro/Ponteiros à structs 2.c
+++ Ponteiro/Ponteiros à structs 2.c
@@ -11,16 +11,16 @@ int main(){
     depois = &agora;
 
     depois -> hora = 20; // há uma ordem de precedência nos sinais. Ponto é executado antes do asterísco pelo compilador
-    depois -> min = 20; // outra maneira de escrever
-    depois -> seg = 20;
+    depois -> min = 80; // outra maneira de escrever
+    depois -> seg = 50;
 
     int somatorio = 100;
 
     struct horario antes;
 
-    antes.hora = somatorio + depois -> seg;
-    antes.min = agora.hora + depois -> min;
-    
+    antes.hora = somatorio + depois-> seg;
+    antes.min = agora.hora + depois-> min;
+    antes.seg = depois-> min + depois-> seg;
     printf("%i:%i:%i", antes.hora, antes.min, antes.seg);
     
     return 0;
