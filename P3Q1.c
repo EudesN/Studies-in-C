6f5c762b6a35320b5a535d143931088037735520
Eudes Nazário <eudesnazario178@gmail.com>
2025-01-06 13:36:07 +0000

    Jan 6, 2025, 10:36 AM

--- P3Q1.c
+++ P3Q1.c
@@ -1,6 +1,14 @@
 # include <stdio.h>
 
 int main(){
-    
+    int M[50][12];
+    int cont = 0;
+
+    for(int l = 0; l < 50; l++){
+        for(int c = 0; c < 12; c++){
+            printf("Informe o número de faltas do aluno %d: ");
+            scanf("%d", &M[l][c]);
+        }
+    }
     return 0;
 }
