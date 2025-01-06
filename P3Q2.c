5f9d991bd1333127c1449c5b7eeee9fa3d242e7d
Eudes Nazário <eudesnazario178@gmail.com>
2025-01-06 22:26:34 +0000

    Jan 6, 2025, 7:26 PM

--- P3Q2.c
+++ P3Q2.c
@@ -4,25 +4,35 @@
         float notaP1;
         float notaP2;
     };
-    struct aluno tAluno;
 
-void insereDados(){
+void insereDados(struct aluno *tAluno){
     printf("Informe o a matricula: ");
-    scanf("%d", tAluno.matricula);
+    scanf("%d", &tAluno -> matricula);
 
     printf("Informe a primeira nota: ");
-    scanf("%f", tAluno.notaP1);
+    scanf("%f", &tAluno -> notaP1);
 
     printf("Informe a segunda nota: ");
-    scanf("%f", tAluno.notaP2);
+    scanf("%f", &tAluno -> notaP2);
 }
 
-void calculaNota(){
+float calculaNota(struct aluno tAluno){
     float precisaNota = 21 - (tAluno.notaP1 + tAluno.notaP2);
     return precisaNota;
 }
 
 int main(){
-    
+    struct aluno tAluno;
+
+    insereDados(&tAluno);
+
+    float nota3 = calculaNota(tAluno);
+
+    if(nota3 <= 10){
+        printf("Para passar precisa tirar ao menos: %.1f\n", nota3);
+    }
+    else{
+        printf("Tente a prova final, se possivel\n");
+    }
     return 0;
 }
