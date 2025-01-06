e847ca5ae958db739e213496b48f73d72d3f9c6b
Eudes Nazário <eudesnazario178@gmail.com>
2025-01-06 21:37:17 +0000

    Jan 6, 2025, 6:37 PM

--- P3Q2.c
+++ P3Q2.c
@@ -6,8 +6,23 @@
     };
     struct aluno tAluno;
 
+void insereDados(){
+    printf("Informe o a matricula: ");
+    scanf("%d", tAluno.matricula);
+
+    printf("Informe a primeira nota: ");
+    scanf("%f", tAluno.notaP1);
+
+    printf("Informe a segunda nota: ");
+    scanf("%f", tAluno.notaP2);
+}
+
+void calculaNota(){
+    float precisaNota = 21 - (tAluno.notaP1 + tAluno.notaP2);
+    return precisaNota;
+}
+
 int main(){
-
-
+    
     return 0;
 }
