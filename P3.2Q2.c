4a8b76bcffae381939262f804ed6bf282c17f36b
Eudes Nazário <eudesnazario178@gmail.com>
2025-01-08 18:20:10 +0000

    Jan 8, 2025, 3:20 PM

--- P3.2Q2.c
+++ P3.2Q2.c
@@ -1,28 +1,27 @@
 #include <stdio.h>
 #define l 400
 #define c 300
-    int funcaoMatriz(int M[l][c]);
+
+int funcaoMatriz(int M[l][c]);
 
 int main(){
     int M[l][c];
-    int menor;
+    int menor = 0;
 
-    while(1){
-        printf("Digite os valores da matriz %dx%d:\n", l, c);
-        for(int i = 0; i < l; i++){
-            for(int j = 0; j < c; j++){
-                scanf("%d", M[i][j]);
-            }
+    printf("Digite os valores da matriz %dx%d:\n", l, c);
+    for(int i = 0; i < l; i++){
+        for(int j = 0; j < c; j++){
+            scanf("%d", &M[i][j]);
         }
-        menor = funcaoMatriz(M);
-        printf("Menor elemento: %.d\n", menor);
-        return 0;
     }
+    menor = funcaoMatriz(M);
+    printf("Menor elemento: %d\n", menor);
+    return 0;
 }
 
 int funcaoMatriz(int M[l][c]){
     int soma = 0;
-    int menor;
+    int menor = M[0][0]; // Inicializa menor com o primeiro elemento da matriz
 
     for(int i = 0; i < l; i++){
         for(int j = 0; j < c; j++){
@@ -35,67 +34,12 @@ int funcaoMatriz(int M[l][c]){
     for(int i = 0; i < l; i++){
         for(int j = 0; j < c; j++){
             if(M[i][j] == menor){
-                for(int k = 0; k < c;k++){
+                for(int k = 0; k < c; k++){
                     soma += M[i][k];
                 }
             }
         }
     }
-    printf("A soma de todas as linhas com menor valor e: %d", soma);
+    printf("A soma de todas as linhas com menor valor e: %d\n", soma);
     return menor;
 }
-
-
-
-
-
-
-//     printf("Digite os valores da matriz %dx%d: ", l, c);
-//     for(int i = 0; i < l; i++){
-//         for(int j = 0; j < c; j++){
-//             scanf("%d", &M[i][j]);
-//         }
-//     }
-
-//     menor = funcaoMatriz(M, lMenores, &qntLinhas);
-//     int soma = 0;
-//     for(int i = 0; i < qntLinhas; i++){
-//         for(int j = 0; j < c; j++){
-//             soma += M[lMenores[i]][j]; 
-//         }
-//     }
-//     printf("A soma dos elementos e: %d", soma);
-//     printf("Menor elemento: %d\n", menor);
-//     return 0;
-// }
-
-// int funcaoMatriz(int M[l][c], int lMenores[], int *qntLinhas){
-//     int menor = M[0][0]; 
-//     *qntLinhas = 0;
-
-//     for(int i = 0; i < l; i++){
-//         for(int j = 0; j < c; j++){
-//             if(M[i][j] < menor){
-//                 menor = M[i][j]; // atualiza o valor do menor
-//                 *qntLinhas = 0;
-//                 lMenores[*qntLinhas] = i; // adiciona a linha com o menor valor
-//                 (*qntLinhas)++; // aumenta a quantidade de linhas com menor valor
-//             }
-//             else if(M[i][j] == menor){ // verigicar se a linha já foi registrada
-//                 int jaRegist = 0;
-
-//                 for(int k = 0; k < l; k++){
-//                     if(lMenores[k] == i){
-//                         jaRegist = 1;
-//                         break;
-//                     }
-//                 }
-//                 if(jaRegist == 0){
-//                     lMenores[*qntLinhas] = i;
-//                     (*qntLinhas)++;
-//                 }
-//             }
-//         }
-//     }
-//     return menor;
-}   
