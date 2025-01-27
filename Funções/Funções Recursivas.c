a6c05de88be81ee71e7b500ed95317e13874ecaf
Eudes Nazário <eudesnazario178@gmail.com>
2025-01-27 18:45:35 +0000

    Jan 27, 2025, 3:45 PM

--- Funções/Funções Recursivas.c
+++ Funções/Funções Recursivas.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 // função recursiva é uma função que chama a ela mesma
 
+
+int main(){
+    int fatorial(int x);
+    
+    int num, res;
+    scanf("%i", &num);
+    res = fatorial(num);
+
+printf("O fatorial e %i", res);
+    return 0;
+}
 int fatorial(int x){
     int res;
     if(x == 0){
@@ -10,12 +21,4 @@ int fatorial(int x){
         res = x * fatorial(x - 1);
     }
     return res;
-}
-int main(){
-    int num, res;
-    scanf("%i", &num);
-    res = fatorial(num);
-
-printf("O fatorial e %i", res);
-    return 0;
 }
