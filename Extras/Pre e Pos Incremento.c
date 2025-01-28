b23f1f56f3c8f2dd5524ecdb752ceaab6261ad0c
Eudes Nazário <eudesnazario178@gmail.com>
2025-01-28 14:06:31 +0000

    Jan 28, 2025, 11:06 AM

---
+++ Extras/Pre e Pos Incremento.c
@@ -0,0 +1,12 @@
+// Pré-incremento : Incrementa o valor da variável antes que ela seja usada na expressão.
+// Pós-incremento : Incrementa o valor da variável depois que ela é usada na expressão.
+
+#include <stdio.h>
+
+int main(){
+    int x = 1;
+    int y = 0;
+    
+}
+
+
