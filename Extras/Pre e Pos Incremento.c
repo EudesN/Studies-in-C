5e5fd615b49a4fb1d78e00e5ea5acf425613a932
Eudes Nazário <eudesnazario178@gmail.com>
2025-01-28 20:48:47 +0000

    Jan 28, 2025, 5:48 PM

--- Extras/Pre e Pos Incremento.c
+++ Extras/Pre e Pos Incremento.c
@@ -7,9 +7,13 @@ int main(){
     int x = 1;
     int y = 0;
     
+    y = ++x; // a var x é encrementada em 1 e depois y recebe esse valor
+
+    y = x++; // y recebe o valor inicial de x e depois x é acrescentado em 1
+
     printf("X = %i", x);
     printf("Y = %i", y);
-
+    return 0;
 }
 
 
