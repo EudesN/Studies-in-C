4aad7373c8739aacb613188cbfa89d0a4e29bf97
Eudes Nazário <eudesnazario178@gmail.com>
2025-01-29 18:03:01 +0000

    Jan 29, 2025, 3:03 PM

--- Pré-Processamento/Comando define.c
+++ Pré-Processamento/Comando define.c
@@ -1,7 +1,22 @@
+// Pré-processamento: antes de iniciar a compilação do nosso código-fonte, o arquivo é processado por um pré-processador. 
+//O arquivo de código-fonte é con￾vertido em outro arquivo de código-fonte “expandido”. 
+//Nessa etapa, ocorrem a remoção dos comentários e a interpretação das diretivas de compilação utilizadas, 
+//as quais se iniciam com #.
+
 # include <stdio.h>
 # define SIM 100
-# define NAO = 100
+# define NAO 100
 
 int main(){
+    int x;
+    printf("Informe um número: ");
+    scanf("%d", &x);
 
+    if(x >= 10){
+        printf("%i", SIM);
+    }
+    else{
+        printf("%i", NAO);
+    }
+    return 0;
 }
