f25f9409fa5208b0ca7a16dc49211e7727eb436f
Eudes Nazário <eudesnazario178@gmail.com>
2025-01-29 14:26:47 +0000

    Jan 29, 2025, 11:26 AM

--- Ponteiro/Ponteiros e Vetores.c
+++ Ponteiro/Ponteiros e Vetores.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
 int main (){
-    
+    int vetor[3] = {1, 2, 3};
+    int *ponteiro1 = &vetor; // o ponteiro está apontando para o endereço de memoria do primeiro membro do vetor
+    int *ponteiro2 = & vetor[1];
+
+    printf("%i", ponteiro1);
+    printf("%i", ponteiro2);
     return 0;
 }
