60dce326fcfd2b0aca9bf74c6c18183e410f29b3
Eudes Nazário <eudesnazario178@gmail.com>
2025-02-01 02:47:09 +0000

    Jan 31, 2025, 11:47 PM

---
+++ Pré-Processamento/Comando define 2.c
@@ -0,0 +1,17 @@
+# include <stdio.h>
+# define PI 3.14159
+# define Nulo 0
+
+double AreaCirculo(double raio){
+    return raio * raio * PI;
+}
+
+int main(){
+    double r;
+    double AreaCirculo(double raio);
+
+    printf("Informe o valor do raio: ");
+    scanf("%d", &r);
+
+    return 0;
+}
