3726e3725381de87ec95fe65051d4745f682fdf3
Eudes Nazário <eudesnazario178@gmail.com>
2025-02-01 03:23:23 +0000

    Feb 1, 2025, 12:23 AM

--- Pré-Processamento/Comando define 2.c
+++ Pré-Processamento/Comando define 2.c
@@ -1,17 +1,21 @@
 # include <stdio.h>
 # define PI 3.14159
-# define Nulo 0
+# define NULO 0
 
+// oadaodmaodowadowadmod
 double AreaCirculo(double raio){
     return raio * raio * PI;
 }
 
 int main(){
-    double r;
+    double r ;
     double AreaCirculo(double raio);
+    int i = 3;
 
+while(i != NULO){
     printf("Informe o valor do raio: ");
-    scanf("%d", &r);
-
+    scanf("%lf", &r);
+    printf("A area do circulo e %lf\n", AreaCirculo(r));
+}
     return 0;
 }
