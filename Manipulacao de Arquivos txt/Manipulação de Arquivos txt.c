045fe7cdabeb0ecf94ba802f8399f163d4614093
Eudes Nazário <eudesnazario178@gmail.com>
2025-02-02 22:08:34 +0000

    Feb 2, 2025, 7:08 PM

---
+++ Manipulacao de Arquivos txt/Manipulação de Arquivos txt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    FILE *file = fopen("C:\\Progamacao\\C\\Manipulacao de Arquivos txt\\ teste.txt", "w"); 
+    // "r" = read(ler informações) ; "w" write (escrever no arquivo) ; "a" append(alterar informações)
+    // existem outras variações
+    // a função fopen recebe como argumentos local e nome do arquivo
+    if(file == NULL){
+        printf("Erro ao abrir o arquivo\n");
+        return 1;
+    }
+    fprintf(file, "Exemplo de escrita em um arquivo usando C\n");
+
+    fclose(file);
+    printf("Arquivo criado com sucesso\n");
+    return 0;
+}
