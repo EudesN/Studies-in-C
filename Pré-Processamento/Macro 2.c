3e767dad7e2b55127553b44121803449f41fd00b
Eudes Nazário <eudesnazario178@gmail.com>
2025-02-02 18:26:50 +0000

    Feb 2, 2025, 3:26 PM

--- Pré-Processamento/Macro 2.c
+++ Pré-Processamento/Macro 2.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 #define MAIOR(x, y) x > y ? x : y
+#define E_MINUSCULO(char) char >= 'a' && char <= 'z' // 
 
 int main(){
+    char X = 'a';
 
+    if(E_MINUSCULO(X)){
+        printf("E uma letra minuscula\n");
+    }
+    else{
+        printf("Nao e uma letra minuscula\n");
+    }
+    printf("%i\n", MAIOR(10, 50));
     return 0;
 }
