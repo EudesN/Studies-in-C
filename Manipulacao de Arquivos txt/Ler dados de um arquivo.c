2a9abb48f71c39a1e50bbeab80e8f65dfb4bb406
Eudes Nazário <eudesnazario178@gmail.com>
2025-02-03 04:08:01 +0000

    Feb 3, 2025, 1:08 AM

---
+++ Manipulacao de Arquivos txt/Ler dados de um arquivo.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int x, y, z;
+    FILE *file;
+    FILE *sFile;
+    file = fopen("C:\\Progamacao\\C\\Manipulacao de Arquivos txt\\numero.txt", "r");
+
+    if(file == NULL){
+        printf("Arquivo 'numero.txt nao foi encontrado.\n");
+        return 1;
+    }
+    fscanf(file, "%i %i %i", &x, &y, &z);
+    printf("%i %i %i\n", x, y, z);
+    fclose(file);
+    
+    sFile = fopen("C:\\Progamacao\\C\\Manipulacao de Arquivos txt\\string.txt", "r");
+    if(sFile == NULL){
+        printf("Arquivo 'string.txt' nao foi encontrado.\n");
+        exit(0);
+    }
+
+    char frase[100];
+    while(fgets(frase, 100, sFile) != NULL){
+        printf("%s", frase);
+    }
+    fclose(sFile);
+    return 0;
+}
