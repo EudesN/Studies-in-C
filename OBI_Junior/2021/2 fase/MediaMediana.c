636c345fd4a9b5bb5287bc8e62b49fee748ab808
Eudes Nazário <eudesnazario178@gmail.com>
2025-12-07 20:25:45 +0000

    Dec 7, 2025, 5:25 PM

--- OBI_Junior/2021/2 fase/MediaMediana.c
+++ OBI_Junior/2021/2 fase/MediaMediana.c
@@ -13,19 +13,19 @@ int main(){
         }
     }
 
-    while (1){
-        c++;
-        media = (a+ b +c) / 3;
-        if((a <= b && b <= c) || (c <= b &&  b <= a)){
-            mediana = b;
-        } else if(( b <= a && a <= c) || (c <= a && a <= b)){
-            mediana = a;
-        } else{
-            mediana = c;
-        }
-        if(media == mediana){
-            break;
-        }
+    c = 2*a + b;
+    media = (a + b + c) / 3;
+    media = (a+ b +c) / 3;
+    if((a <= b && b <= c) || (c <= b &&  b <= a)){
+        mediana = b;
+    } else if(( b <= a && a <= c) || (c <= a && a <= b)){
+        mediana = a;
+    } else if((a <= c && c <= b) || (b <= c)){
+        mediana = c;
     }
-    printf("%d", c );
+
+    if(media == mediana){
+        printf("%d", c );
+    }
+
 }
