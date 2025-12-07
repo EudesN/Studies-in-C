5112656a8091b9bd6adfc33f22e5a76b4173bab0
Eudes Nazário <eudesnazario178@gmail.com>
2025-12-07 19:49:36 +0000

    Dec 7, 2025, 4:49 PM

--- OBI Nível Júnior/2021/2 fase/MediaMediana.c
+++ OBI Nível Júnior/2021/2 fase/MediaMediana.c
@@ -1,20 +1,32 @@
 # include <stdio.h>
 
 int main(){
-    int a,b,c;
+    int a,b;
+    int c = 0;
     int media, mediana;
 
-    while (1)
-    {
+    while (1){
         scanf("%f", &a);
         scanf("%f", &a);
-        if(a <= 1 || b >= 100000000){
-            
+        if(a >= 1 && b <= 100000000){
+            break;
         }
+        break;
     }
     
-    
-
-
-
+    while (1){
+        c++;
+        media = (a + b + c) / 3;
+        if((a <= b && b <= c) || (c <= b &&  b <= a)){
+            mediana = b;
+        } else if(( b <= a && a <= c) || (c <= a && a <= b)){
+            mediana = a;
+        } else{
+            mediana = c;
+        }
+        if(media = mediana){
+            break;
+        }
+    }
+    printf("%d", c );
 }
